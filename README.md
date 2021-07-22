6a568fcb97923fb96ebd737b3a435a02c251474e
stscoundrel <sampo@pixels.fi>
2021-07-22 10:08:02 +0300

    Add c++ demo of 2038 problem

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# year-2038-problem-c-
-Demonstration of year 2038 problem in C++
+# Year 2038 problem
+
+Demonstration of year 2038 problem in C++.
+
+The year 2038 problem refers to maximum unix date that can be stored in i32 value. 03:14:07 UTC on 19 January 2038, the integer will overflow. Result will be incorrect time calculation, around 20:45:52 on Friday, 13 December 1901.
+
+## The test
+
+- Creates maximum value for i32
+- Increase it by one
+- In very simplified way, parse current year from the timestamp. Ignores constraints like leap years, leap seconds etc.
+
+## To run
+
+`g++ -o 2038 src/main.cpp`
+`./2038`
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int get_current_year(int timestamp) {
+    int const START_YEAR = 1970;
+    int timestamp_in_years = timestamp / 60 / 60 / 24 / 365;
+
+    return START_YEAR + timestamp_in_years;
+}
+
+int main() {
+    int max_timestamp = 2147483647;
+    int past_max_timestamp = max_timestamp + 1;
+
+    int max_date = get_current_year(max_timestamp);
+    int past_max_date = get_current_year(past_max_timestamp);
+
+    printf("---------------------------------------------------------------- \n");
+    printf("Should print year from max timestamp. Real value should be 2038 \n");
+    printf("Max timestamp: %d \n", max_date); 
+    printf("---------------------------------------------------------------- \n");
+    printf("Should print year from max timestamp + 1 second. Logically it should still be 2038 \n");
+    printf("Past max timestamp: %d \n", past_max_date);
+    printf("Due to integer overflow, we're in 1900s. With proper date calc it would be 1901, but less-than-precise check brings us to 1092. \n");
+    printf("Still far cry from 2038, which would be the 'logical' value \n");
+    printf("---------------------------------------------------------------- \n");
+}
